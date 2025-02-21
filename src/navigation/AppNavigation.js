63f3ba13b6fabeccf547e9bdec3ee6702f56b762
luis martinez <lushiru@hotmail.com>
2025-02-21 12:12:14 -0300

    primer comit

---
+++ src/navigation/AppNavigation.js
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export function AppNavigation() {
+  return (
+    <View>
+      <Text>AppNavigation</Text>
+      <Text>AppNavigation</Text>
+      <Text>AppNavigation</Text>
+    </View>
+  )
+}
---
+++ src/styles/form.js
@@ -0,0 +1,23 @@
+import { StyleSheet } from "react-native";
+
+export const form = StyleSheet.create({
+  input: {
+    marginBottom: 20,
+  },
+  btnSubmit: {
+    padding: 5,
+    backgroundColor: "#0098d3",
+  },
+  btnFile: {
+    padding: 15,
+    borderRadius: 10,
+    backgroundColor: "#3ce7cf",
+    marginBottom: 15,
+  },
+  btnText: {
+    marginTop: 10,
+  },
+  btnTextLabel: {
+    color: "#000",
+  },
+});
